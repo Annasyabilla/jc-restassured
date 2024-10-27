5831f26d7bef464d62a952edd1fc791c97421dfd
Annasyabilla <syabilla90@gmail.com>
2024-10-27 19:46:20 +0700

    Initial commit

---
+++ src/test/java/com/juaracoding/MovieListsTest.java
@@ -0,0 +1,72 @@
+package com.juaracoding;
+/*
+IntelliJ IDEA 2024.1.4 (Ultimate Edition)
+Build #IU-241.18034.62, built on June 21, 2024
+@Author Lenovo Gk a.k.a. Anna Syabilla
+Java Developer
+Created on 10/27/2024 3:45 PM
+@Last Modified 10/27/2024 3:45 PM
+Version 1.0
+*/
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class MovieListsTest {
+    String baseUrl = "https://api.themoviedb.org/3/movie/";
+    String myToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDkxOWE3YTI5Y2M2YTg2OTcxNGUwODZkNWNiZWU2YiIsIm5iZiI6MTcyOTg4OTk3Ny43NjU4NzMsInN1YiI6IjY3MWEyM2ZjNWJlOWU4NzU5ZGE2ZTk4YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jObqflaP482fLLQ5vrOWXoW-AX2STDTNeo5o88g79rk";
+
+
+    @Test
+    public void testNowPlaying() {
+        given()
+                .queryParam("language", "en-US")
+                .queryParam("page", "1")
+                .header("Authorization", "Bearer " + myToken)
+                .when()
+                .get(baseUrl + "now_playing")
+                .then()
+                .statusCode(200)
+                .body("results.title[0]",equalTo("Venom: The Last Dance"))
+                .body("results.title", everyItem(notNullValue()))
+                .log().all();
+
+    }
+
+    @Test
+    public void testMoviePopular() {
+        given()
+                .queryParam("language", "en-US")
+                .queryParam("page", "1")
+                .header("Authorization", "Bearer " + myToken)
+                .when()
+                .get(baseUrl + "popular")
+                .then()
+                .statusCode(200)
+                .body("results.title[0]",equalTo("Venom: The Last Dance"))
+                .body("results", notNullValue())
+                .body("results.title", everyItem(notNullValue()))
+                .log().all();
+    }
+
+
+    //negative test
+    @Test
+    public void testWithoutToken() {
+        given()
+                .queryParam("language", "en-US")
+                .queryParam("page", "1")
+                .when()
+                .get(baseUrl + "popular")
+                .then()
+                .statusCode(401)
+                .log().all();
+    }
+}
---
+++ src/test/java/com/juaracoding/MoviesTest.java
@@ -0,0 +1,71 @@
+package com.juaracoding;
+/*
+IntelliJ IDEA 2024.1.4 (Ultimate Edition)
+Build #IU-241.18034.62, built on June 21, 2024
+@Author Lenovo Gk a.k.a. Anna Syabilla
+Java Developer
+Created on 10/27/2024 5:49 PM
+@Last Modified 10/27/2024 5:49 PM
+Version 1.0
+*/
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class MoviesTest {
+    String baseUrl = "https://api.themoviedb.org/3/movie/";
+    String myToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMDkxOWE3YTI5Y2M2YTg2OTcxNGUwODZkNWNiZWU2YiIsIm5iZiI6MTcyOTg4OTk3Ny43NjU4NzMsInN1YiI6IjY3MWEyM2ZjNWJlOWU4NzU5ZGE2ZTk4YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jObqflaP482fLLQ5vrOWXoW-AX2STDTNeo5o88g79rk";
+
+    @Test
+    public void testDetails(){
+        int movieId = 912649; // ID film yang ingin diberi rating
+        given()
+                .pathParams("movie_id", movieId)
+                .queryParam("language", "en-US")
+                .header("Authorization", "Bearer " + myToken)
+                .when()
+                .get(baseUrl+"{movie_id}")
+                .then()
+                .statusCode(200)
+                .body("title", equalTo("Venom: The Last Dance"))
+                .body("release_date", equalTo("2024-10-22"))
+                .body("status", equalTo("Released"))
+                .body("tagline", equalTo("'Til death do they part."))
+                .log().all();
+    }
+
+    @Test
+    public void testDetailsNotFound(){
+        int movieId = 1;
+        given()
+                .pathParams("movie_id", movieId)
+                .queryParam("language", "en-US")
+                .header("Authorization", "Bearer " + myToken)
+                .when()
+                .get(baseUrl+"{movie_id}")
+                .then()
+                .statusCode(404)
+                .log().all();
+    }
+
+
+
+    @Test
+    public void testAddRating() {
+        int movieId = 912649;
+        double ratingValue = 8.5;
+
+        given()
+                .pathParams("movie_id", movieId)
+                .header("Authorization", "Bearer " + myToken)
+                .contentType("application/json;charset=utf-8")
+                .body("{\"value\":" + ratingValue + "}")
+                .when()
+                .post(baseUrl + "{movie_id}/rating")
+                .then()
+                .statusCode(201)
+                .log().all();
+    }
+}
